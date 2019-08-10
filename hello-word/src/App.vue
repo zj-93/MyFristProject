9109f8817c930067dc6022c5cdb3aae56fbe8271
zhoujun <zhoujun_1993@163.com>
2019-08-10 16:44:20 +0800

    图片展示以及排序拖拽分类

--- hello-word/src/App.vue
+++ hello-word/src/App.vue
@@ -1,28 +1,150 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <label>
+      <input name="Fruit" type="radio" value checked="checked" @click="changeBtn('date')" />时间
+    </label>
+    <label>
+      <input name="Fruit" type="radio" value @click="changeBtn('type')" />类别
+    </label>
+    <div>
+      <div class="content" v-if="xFlag">
+        <div
+          class="bigList"
+          v-for="(item, index) in list"
+          :key="index"
+        >
+          <div class="title" v-if="filterFlag == 'date'">{{item[0].date}}</div>
+          <div class="title" v-if="filterFlag == 'type'">{{item[0].type}}</div>
+          <div class="dragContent"
+          :data-radius="item[0].type"
+          @drop="drop($event)"
+          @dragover="allowDrop($event)">
+            <div class="smallList" v-for="(ele) in item" :key="ele.id">
+              <img
+              class="imglist"
+                @dragstart="dragStart($event)"
+                @drag="dragging($event)"
+                draggable="true"
+                :id="ele.id"
+                :src="ele.imgUrl"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import draggable from "vuedraggable";
+import listJson from "@/json/list.json";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    draggable
+  },
+  data() {
+    return {
+      initialList: [],
+      list: [],
+      filterFlag: "date",
+      xFlag: true
+    };
+  },
+  computed: {
+    dragOptions() {
+      group: this.initialList;
+    }
+  },
+  created() {},
+  mounted() {
+    localStorage.setItem('listJson', JSON.stringify(listJson))
+    this.initialList = listJson;
+    this.filterFun();
+  },
+  methods: {
+    dragStart(event) {
+      event.dataTransfer.setData("Text", event.target.id);
+    },
+    dragging(event) {
+      // document.getElementById("demo").innerHTML = " p 元素正在拖动";
+    },
+    allowDrop(event) {
+      console.log(111111111)
+      event.preventDefault();
+    },
+    drop(event) {
+      event.preventDefault();
+      var data = event.dataTransfer.getData("Text");
+      // if(event.toElement.className == "dragContent") {
+        event.currentTarget.insertBefore(document.getElementById(data).parentElement, data.parentElement)
+        let item = this.initialList.find(ele => ele.id == data)
+        let itemType = event.target.parentElement.parentElement.dataset.radius || event.target.dataset.radius
+        item.type = itemType
+        console.log(this.initialList, item, itemType)
+        localStorage.setItem('listJson', JSON.stringify(this.initialList))
+        this.filterFun()
+      // } else {
+      //   event.stopPropagation()
+      // }
+      
+      // document.getElementById(data).parentElement.remove();
+    },
+    filterFun() {
+      let list = JSON.parse(localStorage.getItem('listJson'))
+      let arr = [];
+      let arrList = [];
+      list.forEach(ele => {
+        if (arr.indexOf(ele[this.filterFlag]) == -1) {
+          arr.push(ele[this.filterFlag]);
+          arrList.push(
+            list.filter(item => item[this.filterFlag] === ele[this.filterFlag])
+          );
+        }
+      });
+      this.list = arrList;
+      console.log(arrList);
+    },
+    changeBtn(val) {
+      this.xFlag = false;
+      let timer = setTimeout(() => {
+        this.xFlag = true;
+        clearTimeout(timer);
+      }, 0)
+      this.filterFlag = val;
+      this.filterFun();
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.title {
+  text-align: left;
+  margin-bottom: 20px;
+}
+.bigList {
+  text-align: left;
+}
+.smallList {
+  display: inline-block;
+  width: 300px;
+  height: 250px;
+  margin-right: 20px;
+}
+img {
+  display: inline-block;
+  width: 300px;
+  height: 250px;
 }
 </style>
